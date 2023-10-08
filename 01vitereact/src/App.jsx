6521b05b5d4709d73b1b8e36042830b96f1640f1
Anand <akanand104@gmail.com>
2023-10-08 10:35:00 +0530

    learnt about how elemnt cnverted into tree in react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 import Chai from "./chai";
 
 function App() {
+  const userName = "Anand"
   return (
     <>
       <Chai />
-      <h1>Chai aur react with vite</h1>
+      <h1>Chai aur react with vite | {userName}</h1>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // no variable named root is created like react 
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Hi i am from function created directly in main.jsx file</h1>
+    </div>
+  );
+}
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    {" "}
+    Visit google
+  </a>
+); // this will get converted into object tree model by react ad then it will render - to make this render remove all other render and keep only this
+const anotherUser = "chai and react of another user" // how does the variable of js get inject in React
+
+
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "Click me to visit google",
+  anotherUser // after the tree model created the variable gets injected as it is 
+);
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // no variable named root is created like react
+  //<React.StrictMode>
+  //anotherElement
+   reactElement
+  // <>
+  //   <App />
+  //   <MyApp />
+  // </>
+
+  //</React.StrictMode>
+);
