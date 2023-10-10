52dc175be02e508163a695ef4293d80b7f000144
Anand <akanand104@gmail.com>
2023-10-10 06:01:11 +0530

    interview question added

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,48 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  let [counter, setCounter] = useState(15)
+  const [counter, setCounter] = useState(15);
   // let counter = 10;
 
   const addValue = () => {
     console.log("value added", Math.random());
     // counter = counter+1
 
-    if (counter == 20) {
-      counter = 20
-    } else {
-      setCounter(counter + 1)
-    }
-   
-  }
+    // if (counter == 20) {
+    //   counter = 20
+    // } else {
+    //   setCounter(counter + 1)
+    // }
+
+    // interview question - what will happen if we write below code 5 times
+
+    /* setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1); */
+
+    setCounter(prevCounter => prevCounter +1);
+    //using callback fn to get the Above result.All the call back function will be executed then state will be updated
+    setCounter(prevCounter => prevCounter +1+1);
+    setCounter(prevCounter => prevCounter +1);
+    setCounter(prevCounter => prevCounter +1);
+  };
 
   const removeValue = () => {
     console.log("value added", Math.random());
     if (counter == 0) {
-      counter = 0
+      counter = 0;
     } else {
-      setCounter(counter - 1)
+      setCounter(counter - 1);
     }
-   
-  }
+  };
 
   return (
     <>
       <h1>chai and react</h1>
       <h3>counter value {counter}</h3>
 
-      <button 
-      onClick={addValue} //passing the reference to the function
-      >Add value {counter}</button>
-      <br />
       <button
-      onClick={removeValue}
-      >Remove Value {counter}</button>
+        onClick={addValue} //passing the reference to the function
+      >
+        Add value {counter}
+      </button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
