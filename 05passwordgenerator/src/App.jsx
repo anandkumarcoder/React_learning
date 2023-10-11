09cf2a1c33fefe8afa80453feb5ee3ccaa09046d
Anand <akanand104@gmail.com>
2023-10-12 00:46:57 +0530

    noram working logic created without useEffect

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useCallback } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
@@ -21,13 +20,87 @@ function App() {
     if (numberAllowed) str += "1234567890";
     if (charAllowed) str += "!@#$%^&*_-+=[]{}~`";
 
+    for (let i = 0; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      // console.log(char);
+      pass = str.charAt(char)
+
+      setPassword(pass)
+      
+    }
+
 
 
   }, [length, numberAllowed, charAllowed, setPassword])
 
   return (
     <>
-      <h1 className="text-4xl bg-black text-white">Password Generator</h1>
+      <div className="w-full max-w-md max-auto shadow-md rounded-lg text-orange-500 bg-gray-700 px-4 my-8 py-3 " >
+<h1 className="text-white text-center mb-5">Password Generator</h1>
+
+      <div className="flex shadow rounded-lg mb-4 overflow-hidden">
+
+<input type="text"
+value={password}
+className="outline-none w-full py-1 px-3" 
+placeholder="password" readOnly
+/>
+
+<button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">Copy</button>
+</div>
+
+<div className=" flex text-sm gap-x-2">
+
+<div className=" flex item-center gap-x-1">
+  <input 
+  type="range" 
+  min={6}
+  max = {100}
+  value = {length}
+  className="cursor-pointer"
+  onChange={(e) => {setLength(e.target.value)}}
+  />
+
+<label >Length: {length}</label>
+
+</div>
+
+
+<div className=" flex item-center gap-x-1">
+  <input 
+  type="checkbox" 
+  defaultChecked={numberAllowed}
+  id = "numberInput"
+  onChange={ () => {
+    setNumberAllowed((prev => !prev))
+    // this is for changing the checkbox again and again
+  }}
+  />
+<label htmlFor="numberInput">Numbers</label>
+
+
+</div>
+
+<div className=" flex item-center gap-x-1">
+  <input 
+  type="checkbox" 
+  defaultChecked={charAllowed}
+  id = "characterInput"
+  onChange={ () => {
+    setCharAllowed((prev => !prev))
+    // this is for changing the checkbox again and again
+  }}
+  />
+<label htmlFor="characterInput">Character</label>
+
+
+</div>
+</div>
+      </div>
+
+      
+
+
     </>
   );
 }
